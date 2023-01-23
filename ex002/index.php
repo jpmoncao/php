7be767b714f4083d5bdb51d853fae55c91917d6d
João Pedro Monção <pedrohjoao44@gmail.com>
2023-01-23 14:34:11 -0300

    ✅ Concatenação de strings com HTML e PHP

---
+++ ex002/index.php
@@ -0,0 +1,21 @@
+<?php
+    $name = 'João Pedro';
+    $lastname = 'Monção';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 03</title>
+</head>
+
+<body>
+    <h3>Meu nome é:</h3>
+    <h1><?= $name . " " . $lastname ?></h1>
+</body>
+
+</html>
